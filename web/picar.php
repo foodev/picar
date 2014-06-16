bf3ce26b94b7140265d42696080cccdf2b37d3d3
foodev <jonas@foodev.de>
2014-06-17 00:57:03 +0200

    fixed case-sensitive filename

--- web/picar.php
+++ web/picar.php
@@ -3,7 +3,7 @@
 $action = isset($_POST['action']) ? $_POST['action'] : null;
 $direction = isset($_POST['direction']) ? $_POST['direction'] : null;
 
-$cmd = 'sudo python /home/pi/picar/scripts/picarControl.py %s %s';
+$cmd = 'sudo python /home/pi/picar/scripts/PiCarControl.py %s %s';
 
 exec(sprintf($cmd, $action, $direction), $output);
 
