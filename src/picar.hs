e565504df96d1d3c4cd31ffddf0d0cb4707a3b10
foodev <jonas@foodev.de>
2014-08-04 22:59:07 +0200

    Haskell source code cleanup.

--- src/picar.hs
+++ src/picar.hs
@@ -4,73 +4,68 @@ motorFront = (4, 17)
 motorBack = (18, 23)
 
 writeToGPIO :: Int -> String -> String -> IO ()
-writeToGPIO pin command value = do
-    writeFile filename value
+writeToGPIO pin command value = writeFile filename value
     where
         filename = "/sys/class/gpio/gpio" ++ show pin ++ "/" ++ command
 
 exportGPIO :: Int -> IO ()
-exportGPIO pin = do
-    writeFile "/sys/class/gpio/export" (show pin)
+exportGPIO pin = writeFile "/sys/class/gpio/export" (show pin)
 
 unexportGPIO :: Int -> IO ()
-unexportGPIO pin = do
-    writeFile "/sys/class/gpio/unexport" (show pin)
+unexportGPIO pin = writeFile "/sys/class/gpio/unexport" (show pin)
 
 setGPIOValue :: Int -> String -> IO ()
-setGPIOValue pin value = do
-    writeToGPIO pin "value" value
+setGPIOValue pin value = writeToGPIO pin "value" value
 
 setGPIODirection :: Int -> String -> IO ()
-setGPIODirection pin value = do
-    writeToGPIO pin "direction" value
+setGPIODirection pin value = writeToGPIO pin "direction" value
 
 driveForward :: String -> IO ()
 driveForward action
     -- drive forward
     | action == "start" = do
-            setGPIOValue (fst motorBack) "1"
-            setGPIOValue (snd motorBack) "0"
+        setGPIOValue (fst motorBack) "1"
+        setGPIOValue (snd motorBack) "0"
     -- stop driving forward
     | action == "stop" = do
-            setGPIOValue (fst motorBack) "0"
-            setGPIOValue (snd motorBack) "0"
+        setGPIOValue (fst motorBack) "0"
+        setGPIOValue (snd motorBack) "0"
     | otherwise = printHelp
 
 driveBack :: String -> IO ()
 driveBack action
     -- drive back
     | action == "start" = do
-            setGPIOValue (fst motorBack) "0"
-            setGPIOValue (snd motorBack) "1"
+        setGPIOValue (fst motorBack) "0"
+        setGPIOValue (snd motorBack) "1"
     -- stop driving back
     | action == "stop" = do
-            setGPIOValue (fst motorBack) "0"
-            setGPIOValue (snd motorBack) "0"
+        setGPIOValue (fst motorBack) "0"
+        setGPIOValue (snd motorBack) "0"
     | otherwise = printHelp
 
 driveLeft :: String -> IO ()
 driveLeft action
     -- drive left
     | action == "start" = do
-            setGPIOValue (fst motorFront) "0"
-            setGPIOValue (snd motorFront) "1"
+        setGPIOValue (fst motorFront) "0"
+        setGPIOValue (snd motorFront) "1"
     -- stop driving left
     | action == "stop" = do
-            setGPIOValue (fst motorFront) "0"
-            setGPIOValue (snd motorFront) "0"
+        setGPIOValue (fst motorFront) "0"
+        setGPIOValue (snd motorFront) "0"
     | otherwise = printHelp
 
 driveRight :: String -> IO ()
 driveRight action
     -- drive right
     | action == "start" = do
-            setGPIOValue (fst motorFront) "1"
-            setGPIOValue (snd motorFront) "0"
+        setGPIOValue (fst motorFront) "1"
+        setGPIOValue (snd motorFront) "0"
     -- stop driving right
     | action == "stop" = do
-            setGPIOValue (fst motorFront) "0"
-            setGPIOValue (snd motorFront) "0"
+        setGPIOValue (fst motorFront) "0"
+        setGPIOValue (snd motorFront) "0"
     | otherwise = printHelp
 
 printHelp :: IO ()
