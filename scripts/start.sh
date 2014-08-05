aa4923db928955088352ffade3421724fbd6a670
foodev <jonas@foodev.de>
2014-08-05 20:57:44 +0200

    Code cleanup of the start script
    Merged the `stream.sh` into the `start.sh` script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-`/home/pi/picar/bin/picar export`
-`/home/pi/picar/scripts/stream.sh`
-`python /home/pi/picar/scripts/startserver.py 8080`
+# export the pins
+/home/pi/picar/bin/picar export
+
+# init the webcam to make every 2 seconds a picture
+fswebcam -b -r 1280x720 --jpeg 95 --no-banner -q -l 2 /home/pi/picar/web/stream.jpg
+
+# start the webserver
+python /home/pi/picar/scripts/startserver.py 8080
--- scripts/stream.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-fswebcam -b -r 1280x720 --jpeg 95 --no-banner -q -l 2 /home/pi/picar/web/stream.jpg
